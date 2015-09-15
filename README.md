6be0f601b1410b632b693b5a19417492dbe04644
Eric Freeman <efreeman@1800contacts.com>
2015-09-15 11:17:31 -0600

    Initial commit.

---
+++ README.md
@@ -0,0 +1,2 @@
+# git-rpg
+Earn XP, complete quests, and save the world - all while honing your git skills.
---
+++ src/git-rpg/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace git_rpg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
