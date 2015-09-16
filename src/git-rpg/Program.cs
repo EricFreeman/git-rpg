d8dcc5f4f7de417ed25bef543dae90ccc81af04b
Eric Freeman <efreeman@1800contacts.com>
2015-09-16 11:33:38 -0600

    Spiking out structure of program.

--- src/git-rpg/Program.cs
+++ src/git-rpg/Program.cs
@@ -1,15 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace git_rpg
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            var input = ParseCommand(args);
+
+            var moduleRunner = new ModuleRunner();
+            moduleRunner.Execute(input);
+
+            Console.WriteLine(input);
         }
+
+        private static string ParseCommand(string[] args)
+        {
+            return args.Aggregate(string.Empty, (accumulator, current) => accumulator + current + " ").Trim();
+        }
+    }
+
+    public class ModuleRunner
+    {
+        private readonly List<IModule> _modules;
+
+        public ModuleRunner()
+        {
+            _modules = new List<IModule>
+            {
+                new DefaultModule()
+            };
+        }
+
+        public void Execute(string input)
+        {
+            _modules.First(x => x.IsMatch(input)).Execute(input);
+        }
+    }
+
+    public class DefaultModule : IModule
+    {
+        public bool IsMatch(string input)
+        {
+            return true;
+        }
+
+        public void Execute(string input) { }
+    }
+
+    public interface IModule
+    {
+        bool IsMatch(string input);
+        void Execute(string input);
     }
 }
