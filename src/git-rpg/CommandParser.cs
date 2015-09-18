34feaf48745f930774d97c2efb7f26c9645e8a42
Eric Freeman <efreeman@1800contacts.com>
2015-09-18 11:45:47 -0600

    Added initial spike of command and parse to read it in.

---
+++ src/git-rpg/CommandParser.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using git_rpg.Models;
+
+namespace git_rpg
+{
+    public class CommandParser
+    {
+        public Command Parse(string input)
+        {
+            var commandParts = input.Split(' ').ToList();
+
+            return new Command
+            {
+                Action = commandParts[0],
+                Arguments = string.Join(" ", commandParts.GetRange(1, commandParts.Count - 1))
+            };
+        }
+    }
+}
---
+++ src/git-rpg/Models/Command.cs
@@ -0,0 +1,8 @@
+﻿namespace git_rpg.Models
+{
+    public class Command
+    {
+        public string Action { get; set; }
+        public string Arguments { get; set; }
+    }
+}
